dd1bd0af5c88af6be45e80ae5a76693e444a9dc4
Rajankumarmaruya <rajanmaurya0102@gmail.com>
2024-10-11 17:35:32 +0530

    demo

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
+const cors = require('cors')
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+app.use(cors());
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
